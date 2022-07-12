fa1d7df6d7586369ae660288e2dbb19c1bfc50dc
widcardw <7620969+widcardw@user.noreply.gitee.com>
2022-07-12 20:30:28 +0800

    refactor: change layout

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-import SideBar from './layouts/SideBar.vue'
 </script>
 
 <template>
-  <div flex>
-    <SideBar fixed />
-    <div class="ml-2.5rem">
-      <RouterView />
-    </div>
-  </div>
+  <RouterView font-sans />
 </template>
 
 <style>
@@ -19,8 +12,24 @@ html.dark {
   background: #121212;
   color: grey;
 }
-html, body {
+
+html,
+body {
   margin: 0;
   padding: 0;
+}
+
+::-webkit-scrollbar {
+  width: 5px;
+  height: 5px;
+  background: transparent;
+}
+
+::-webkit-scrollbar-thumb {
+  background: rgba(128, 128, 128, 0.375);
+}
+
+::-webkit-scrollbar-track {
+  background: rgba(128, 128, 128, 0.125);
 }
 </style>
