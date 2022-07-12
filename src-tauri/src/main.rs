2dd4adb5a91f7349835711fbb48c29fa649c6c70
widcardw <7620969+widcardw@user.noreply.gitee.com>
2022-07-12 22:26:23 +0800

    feat: close kbd on closing main window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,23 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
 fn main() {
   tauri::Builder::default()
+    .setup(|app| {
+      let main_window = app.get_window("main").unwrap();
+      let kbd_window = app.get_window("kbd").unwrap();
+      // 当主窗口关闭时键盘窗口也同时关闭
+      main_window.on_window_event(move |event| {
+        match event {
+          tauri::WindowEvent::CloseRequested {..} => {
+            kbd_window.close().unwrap();
+          },
+          _ => {}
+        };
+      });
+      Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
