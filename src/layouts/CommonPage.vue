cf1576d34903637b79b3390e49e36b8b0081d79f
widcardw <7620969+widcardw@user.noreply.gitee.com>
2022-07-12 20:35:04 +0800

    chore: component

---
+++ src/layouts/CommonPage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import SideBar from './SideBar.vue'
+</script>
+
+<template>
+  <div flex>
+    <SideBar fixed />
+    <div class="ml-2.5rem" flex-1>
+      <RouterView />
+    </div>
+  </div>
+</template>
