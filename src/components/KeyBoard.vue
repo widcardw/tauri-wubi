a2f42af3b0efd4b9dee3bc3947eec854d7a10779
widcardw <7620969+widcardw@user.noreply.gitee.com>
2022-07-12 20:32:05 +0800

    chore: keyboard component

---
+++ src/components/KeyBoard.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import OneKey from '../components/OneKey.vue'
+import type { letter } from '../data/kbd'
+import kbd from '../data/kbd'
+
+const line1: letter[] = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p']
+const line2: letter[] = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l']
+const line3: letter[] = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+</script>
+
+<template>
+  <div font-sans flex flex-col>
+    <div flex>
+      <OneKey
+        v-for="c in line1" :key="c"
+        :name="kbd[c].name"
+        :char="kbd[c].char"
+        :strokes="kbd[c].strokes"
+        :code="kbd[c].code"
+        :indicator="kbd[c].indicator"
+      />
+    </div>
+    <div flex class="ml-3rem">
+      <OneKey
+        v-for="c in line2" :key="c"
+        :name="kbd[c].name"
+        :char="kbd[c].char"
+        :strokes="kbd[c].strokes"
+        :code="kbd[c].code"
+        :indicator="kbd[c].indicator"
+      />
+    </div>
+    <div flex class="ml-6rem">
+      <OneKey
+        v-for="c in line3" :key="c"
+        :name="kbd[c].name"
+        :char="kbd[c].char"
+        :strokes="kbd[c].strokes"
+        :code="kbd[c].code"
+        :indicator="kbd[c].indicator"
+      />
+    </div>
+  </div>
+</template>
---
+++ src/components/OneKey.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+  name: string
+  code: number
+  char: string
+  strokes: Array<string>
+  indicator?: string
+}>()
+</script>
+
+<template>
+  <div mkey>
+    <div flex-1 grid grid-cols-4>
+      <span v-for="s in strokes" :key="s" text-center>
+        {{ s }}
+      </span>
+    </div>
+    <div flex justify-center space-x-2>
+      <div w-1rem h-1rem text-teal-600>
+        {{ indicator }}
+      </div>
+      <div self-center font-bold>
+        {{ name.toUpperCase() }}
+      </div>
+      <div self-center>
+        {{ char }}
+      </div>
+    </div>
+  </div>
+</template>
