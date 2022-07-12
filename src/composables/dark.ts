1d909ff2e2d56917815066e3ba9d672bc7313f01
widcardw <7620969+widcardw@user.noreply.gitee.com>
2022-07-12 20:32:37 +0800

    chore: sort imports

--- src/composables/dark.ts
+++ src/composables/dark.ts
@@ -1,5 +1,5 @@
 // these APIs are auto-imported from @vueuse/core
-import { useDark, useToggle, usePreferredDark } from "@vueuse/core"
+import { useDark, usePreferredDark, useToggle } from '@vueuse/core'
 export const isDark = useDark()
 export const toggleDark = useToggle(isDark)
 export const preferredDark = usePreferredDark()
